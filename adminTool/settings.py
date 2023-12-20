293538b3daa72667f9de16191745528b89af9932
Ayesha Rafi <ayesharafi313@gamil.com>
2023-12-21 01:24:36 +0500

    update settings file

--- adminTool/settings.py
+++ adminTool/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 
-# Parse database configuration from $DATABASE_URL
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,6 +94,8 @@ DATABASES = {
     }
 }
 
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
